7ee6cc3fac276487432cb99c84917e91b1dda65e
Elia <98826251+Elia1802@users.noreply.github.com>
2022-07-08 19:46:26 +0200

    Update RulesEvent.java

--- src/main/java/de/elia/ghostmain/thisplugin/events/rules/RulesEvent.java
+++ src/main/java/de/elia/ghostmain/thisplugin/events/rules/RulesEvent.java
@@ -1,6 +1,6 @@
 package de.elia.ghostmain.thisplugin.events.rules;
 
-import de.elia.ghostmain.thisplugin.GhostMain;
+import de.elia.ghostmain.GhostMain;
 import de.elia.ghostmain.all.plugins.prefix.Prefix;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -12,15 +12,15 @@ public class RulesEvent implements Listener {
     @EventHandler
     public void onRules(PlayerJoinEvent event){
         Player player = event.getPlayer();
-        player.sendMessage(Prefix.getGhostMainPrefix() + GhostMain.getInstance().getConfiguration().getString("Rules 1"));
-        player.sendMessage(Prefix.getGhostMainPrefix() + GhostMain.getInstance().getConfiguration().getString("Rules 2"));
-        player.sendMessage(Prefix.getGhostMainPrefix() + GhostMain.getInstance().getConfiguration().getString("Rules 3"));
-        player.sendMessage(Prefix.getGhostMainPrefix() + GhostMain.getInstance().getConfiguration().getString("Rules 4"));
-        player.sendMessage(Prefix.getGhostMainPrefix() + GhostMain.getInstance().getConfiguration().getString("Rules 5"));
-        player.sendMessage(Prefix.getGhostMainPrefix() + GhostMain.getInstance().getConfiguration().getString("Rules 6"));
-        player.sendMessage(Prefix.getGhostMainPrefix() + GhostMain.getInstance().getConfiguration().getString("Rules 7"));
-        player.sendMessage(Prefix.getGhostMainPrefix() + GhostMain.getInstance().getConfiguration().getString("Rules 8"));
-        player.sendMessage(Prefix.getGhostMainPrefix() + GhostMain.getInstance().getConfiguration().getString("Rules 9"));
+        player.sendMessage(Prefix.getGhostMainPrefixOld() + GhostMain.getInstance().getMessageConfiguration().getString("Rules 1"));
+        player.sendMessage(Prefix.getGhostMainPrefixOld() + GhostMain.getInstance().getMessageConfiguration().getString("Rules 2"));
+        player.sendMessage(Prefix.getGhostMainPrefixOld() + GhostMain.getInstance().getMessageConfiguration().getString("Rules 3"));
+        player.sendMessage(Prefix.getGhostMainPrefixOld() + GhostMain.getInstance().getMessageConfiguration().getString("Rules 4"));
+        player.sendMessage(Prefix.getGhostMainPrefixOld() + GhostMain.getInstance().getMessageConfiguration().getString("Rules 5"));
+        player.sendMessage(Prefix.getGhostMainPrefixOld() + GhostMain.getInstance().getMessageConfiguration().getString("Rules 6"));
+        player.sendMessage(Prefix.getGhostMainPrefixOld() + GhostMain.getInstance().getMessageConfiguration().getString("Rules 7"));
+        player.sendMessage(Prefix.getGhostMainPrefixOld() + GhostMain.getInstance().getMessageConfiguration().getString("Rules 8"));
+        player.sendMessage(Prefix.getGhostMainPrefixOld() + GhostMain.getInstance().getMessageConfiguration().getString("Rules 9"));
     }
 
 }
