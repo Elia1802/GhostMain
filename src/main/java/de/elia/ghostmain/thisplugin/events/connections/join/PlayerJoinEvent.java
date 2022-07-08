538d8250160239ee80366b8479454ee7840ec2e9
Elia <98826251+Elia1802@users.noreply.github.com>
2022-07-08 19:49:31 +0200

    Update PlayerJoinEvent.java

--- src/main/java/de/elia/ghostmain/thisplugin/events/connections/join/PlayerJoinEvent.java
+++ src/main/java/de/elia/ghostmain/thisplugin/events/connections/join/PlayerJoinEvent.java
@@ -4,12 +4,13 @@ import de.elia.ghostmain.all.plugins.prefix.Prefix;
 import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
-public class PlayerJoinEvent implements Listener {
+public class JoinEvent implements Listener {
 
     @EventHandler
-    public void onJoin(org.bukkit.event.player.PlayerJoinEvent event){
-        event.setJoinMessage(Prefix.getGhostMainPrefix() + ChatColor.AQUA + event.getPlayer().getName() + " " + ChatColor.GRAY + "joint the Server");
+    public void onJoin(PlayerJoinEvent event){
+        event.setJoinMessage(Prefix.getGhostMainPrefixOld() + ChatColor.AQUA + event.getPlayer().getName() + " " + ChatColor.GRAY + "joint the Server");
     }
 
 }
